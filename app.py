4e6d0ee69cf11e2fb8c302a2c5252a5904b957d5
Daniel Uyo <duyo@ualberta.ca>
2021-01-12 14:34:57 -0700

    A New Version of the Madlib Game Containing More Story Options

--- app.py
+++ app.py
@@ -1,20 +1,30 @@
 from flask import Flask, request, render_template
 from flask_debugtoolbar import DebugToolbarExtension
-from stories import story
+from stories import stories
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "MADLIBS"
 debug = DebugToolbarExtension(app)
 
 @app.route("/")
-def prompt():
-    """This function uses the prompts provided to render the madlib_form.html doc"""
+def list_of_stories():
+    """This function shows the lists of stories"""
+    return render_template("list_of_stories.html", stories = stories.values())
+
+@app.route("/madlib_form")
+def madlib_form():
+    """This function displays form pertaining to story choosen in the home directory"""
+    story_id = request.args["story_id"]
+    story = stories[story_id]
     prompts = story.prompts
-    return render_template("madlib_form.html", prompts = prompts)
+
+    return render_template("madlib_form.html", story_id = story_id, title = story.title, prompts = prompts)
 
 @app.route("/madlib_story")
 def madlib_story():
     """This function renders the information entered in the madlib_form.html into a story in madlib_story.html"""
+    story_id = request.args["story_id"]
+    story = stories[story_id]
     text = story.generate(request.args)
 
-    return render_template("madlib_story.html", text = text)
+    return render_template("madlib_story.html", title = story.title, text = text)
--- stories.py
+++ stories.py
@@ -4,10 +4,10 @@
 class Story:
     """Madlibs story.
 
-    To  make a story, pass a list of prompts, and the text
+    To  make a story, pass a code, a title, alist of prompts, and the text
     of the template.
 
-        >>> s = Story(["noun", "verb"],
+        >>> s = Story("simple", "A simple Tale", ["noun", "verb"],
         ...     "I love to {verb} a good {noun}.")
 
     To generate text from a story, pass in a dictionary-like thing
@@ -18,9 +18,10 @@ class Story:
         'I love to eat a good mango.'
     """
 
-    def __init__(self, words, text):
+    def __init__(self,code, title, words, text):
         """Create story with words and template text."""
-
+        self.code = code
+        self.title = title
         self.prompts = words
         self.template = text
 
@@ -38,22 +39,22 @@ class Story:
 # Here's are some instance stories to get started
 
 
-story = Story(
+story1 = Story("history", "history tale",
     ["place", "noun", "verb", "adjective", "plural_noun"],
     """Once upon a time in a long-ago {place}, there lived a
        large {adjective} {noun}. It loved to {verb} {plural_noun}."""
 )
 
-# story2 = Story(["work_place", "job_postion", "action", "connect"],
-#                 """So I've been applying to {work_place} for the 
-#                     {job_position}. I am hoping to {action} with
-#                     {connect} to discuss further."""
-#                 )
-# story3 = Story(["name", "location", "action"],
-#                 """I am going out with {name}, we will be going to {location}
-#                     to {action}"""
-#                 )
+story2 = Story("work", "Loooking for work", ["work_place", "job_postion", "action", "connect"],
+                """So I've been applying to {work_place} for the 
+                    {job_position}. I am hoping to {action} with
+                    {connect} to discuss further."""
+                )
+story3 = Story("outing", "Spending Time with Friends", ["name", "location", "action"],
+                """I am going out with {name}, we will be going to {location}
+                    to {action}"""
+                )
 
-# #A dict is created of {id:story, id:story}
+#A dict is created of {code:story, code:story}
 
-# stories = {s.id: s for s in [story, story2, story3]}
+stories = {s.code: s for s in [story1, story2, story3]}
